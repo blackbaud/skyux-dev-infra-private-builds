199c0f7f3ac032b929e9e58b2317ac4454fa01fb
John White <john.white@blackbaud.me>
2022-07-15 16:04:49 +0000

    1867e71 chore(release): 1.2.4

--- bin/skyux-dev.js
+++ bin/skyux-dev.js
@@ -13,10 +13,14 @@ const cli_4 = require("../lib/cherry-pick/cli");
 const cli_5 = require("../lib/create-lib-resources/cli");
 const cli_6 = require("../lib/create-packages-dist/cli");
 const cli_7 = require("../lib/create-release/cli");
-const cli_8 = require("../lib/migrate/cli");
-const cli_9 = require("../lib/pristine-commit/cli");
-const cli_10 = require("../lib/publish-packages-dist/cli");
-const cli_11 = require("../lib/test-affected/cli");
+const cli_8 = require("../lib/e2e-workflow/cli");
+const cli_9 = require("../lib/github-pages/cli");
+const cli_10 = require("../lib/github-pr-comment/cli");
+const cli_11 = require("../lib/migrate/cli");
+const cli_12 = require("../lib/pristine-commit/cli");
+const cli_13 = require("../lib/publish-packages-dist/cli");
+const cli_14 = require("../lib/publish-storybook/cli");
+const cli_15 = require("../lib/test-affected/cli");
 const parser = (0, yargs_1.default)((0, helpers_1.hideBin)(process.argv))
     .command(cli_1.CheckLibraryDependenciesModule)
     .command(cli_2.CheckLibraryMissingPeersModule)
@@ -25,10 +29,16 @@ const parser = (0, yargs_1.default)((0, helpers_1.hideBin)(process.argv))
     .command(cli_5.CreateLibraryResourcesModule)
     .command(cli_6.CreatePackagesDistModule)
     .command(cli_7.CreateReleaseModule)
-    .command(cli_8.MigrateModule)
-    .command(cli_9.PristineCommitModule)
-    .command(cli_10.PublishPackagesDistModule)
-    .command(cli_11.TestAffectedModule)
+    .command(cli_8.E2eWorkflowModule)
+    .command(cli_9.GithubPagesMirrorModule)
+    .command(cli_9.GithubPagesRemoveOldPrsModule)
+    .command(cli_9.GithubPagesPublishModule)
+    .command(cli_10.GithubPrCommentModule)
+    .command(cli_11.MigrateModule)
+    .command(cli_12.PristineCommitModule)
+    .command(cli_13.PublishPackagesDistModule)
+    .command(cli_14.PublishStorybookModule)
+    .command(cli_15.TestAffectedModule)
     .help()
     .strict()
     .demandCommand(1, 'Provide a command (or type --help to see a list of commands).')
